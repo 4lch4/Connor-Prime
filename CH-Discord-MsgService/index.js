c0127917ec8f7e87ef31bb0d417c5ae76c50e2fa
Devin Leaman <dleaman212@gmail.com>
2019-08-05 20:13:07 -0500

    Testing Yuma integration

--- CH-Discord-MsgService/index.js
+++ CH-Discord-MsgService/index.js
@@ -7,19 +7,47 @@ const yBot = new Yuma({
 })
 
 module.exports = async function (context, req) {
-  let res = await yBot.connect()
-  context.log(res)
-  context.log('JavaScript HTTP trigger function processed a request.')
+  await yBot.connect()
+  yBot.createMessage('325510453877014529', {
+    embed: {
+      author: 'Connor',
+      description: 'Automated Message',
+      fields: [{
+        inline: true,
+        name: 'Field A',
+        value: 'Field A Value'
+      }, {
+        inline: true,
+        name: 'Field B',
+        value: 'Field B Value'
+      }, {
+        inline: true,
+        name: 'Field C',
+        value: 'Field C Value'
+      }],
+      footer: { text: 'Hey sexy' },
+      title: 'Message Title',
+      image:
+      { url: 'https://i.ytimg.com/vi/DHqTQzv1jkE/maxresdefault.jpg' }
+    }
+  }).then(res => {
+    context.log('Message sent.')
+    context.log(res)
+    context.res({
+      body: 'Message sent.'
+    })
+  })
+  // context.log('JavaScript HTTP trigger function processed a request.')
 
-  if (req.query.name || (req.body && req.body.name)) {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: 'Hello ' + (req.query.name || req.body.name)
-    }
-  } else {
-    context.res = {
-      status: 400,
-      body: 'Please pass a name on the query string or in the request body'
-    }
-  }
+  // if (req.query.name || (req.body && req.body.name)) {
+  //   context.res = {
+  //     // status: 200, /* Defaults to 200 */
+  //     body: 'Hello ' + (req.query.name || req.body.name)
+  //   }
+  // } else {
+  //   context.res = {
+  //     status: 400,
+  //     body: 'Please pass a name on the query string or in the request body'
+  //   }
+  // }
 }
